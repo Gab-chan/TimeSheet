18ed27d43f23c3f9b6092dda517bfc4e193b71f4
NelsonCad <NelsonCad97@gmail.com>
2019-07-30 12:39:58 -0700

    firebase added to js file

---
+++ app.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyB_N-jjhq6nk-mtF0c-Wo43EibouEpvc44",
+        authDomain: "employeelist-bbe16.firebaseapp.com",
+        databaseURL: "https://employeelist-bbe16.firebaseio.com",
+        projectId: "employeelist-bbe16",
+        storageBucket: "",
+        messagingSenderId: "481309067568",
+        appId: "1:481309067568:web:994698336e47f9dd"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    let database = firebase.database();
+
+
+
+
+
+}) 
